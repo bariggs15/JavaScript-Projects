3bfbaebb1053aea4e4ca5030cfa2c8ed08e4a258
bariggs15 <bariggs15@gmail.com>
2021-01-26 13:56:02 -0600

    first part of assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,12 @@
+            
+var A ='this is a string' //practicing incantination/
++" if you keep practing"
++" the better you'll be", D = " math", E = ' tomatoes'; 
+var B ="I am practicing";
+var C ="\"This is tricky sometimes\"";//practicing with quotations
+var E= E.fontcolor("red"); //turns font red 
 
-        <script>
-            window.alert("Hello,world!");
-            document.write('Hello,World');
-        </script>
+    document.write(A, D, E); 
+    document.write(5+14); //this is an expression
+    alert (B);
+    window.alert (C);
