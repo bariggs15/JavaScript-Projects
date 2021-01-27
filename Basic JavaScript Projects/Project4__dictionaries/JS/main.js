50df841ed6f1a88415033dc927e5590538023c96
bariggs15 <bariggs15@gmail.com>
2021-01-27 13:37:02 -0600

    revised

--- Basic JavaScript Projects/Project4__dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4__dictionaries/JS/main.js
@@ -1,9 +1,9 @@
 function my_dictionary() {
     var sports = {
-        Basketball = "Shai Gilgeous Alexander",
-        Football = "Baker Mayfield",
-        Baseball = "Mike Trout",
-        Golf = "Dustin Johnson"
+        Basketball:"Shai Gilgeous Alexander",
+        Football:"Baker Mayfield",
+        Baseball:"Mike Trout",
+        Golf:"Dustin Johnson"
     };
     document.getElementById("demo").innerHTML = sports.Football;
 }
