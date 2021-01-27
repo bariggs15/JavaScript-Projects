d3f273501b4d9eb83537fbdbe0d13339bf9bb2c9
bariggs15 <bariggs15@gmail.com>
2021-01-27 13:16:18 -0600

    comparison type coercion

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,24 @@
+document.write(typeof "Basketball");//type
+
+document.write(5E123456);//infinity
+
+document.write(-9E9876);//-infinity
+
+document.write(100>200);//BOOLEAN
+document.write(100<200);
+
+document.write("40"+3);
+
+document.write(100==200);//checking whether equal true or false
+document.write(100==100);
+
+document.write(111==="111");//comparison between values
+document.write(111===111);
+document.write("onehundredeleven"===111);
+document.write(111===222);
+
+document.write(15>2 && 23>6);//AND
+document.write(15>2 && 23>46);
+document.write(15>2 || 23>6);//OR
+
+document.write(!(20>10));//NOT
